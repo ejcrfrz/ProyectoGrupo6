c0802d9caaa3c0d857e65f0639d4e5a822d46163
Eduardo <eduardo.requena@pucp.pe>
2020-06-11 15:14:00 -0500

    Lista terminadaccon App Bar

---
+++ app/src/main/java/com/pucp/proyectogrupo6/Entidades/Incidencia.java
@@ -0,0 +1,62 @@
+package com.pucp.proyectogrupo6.Entidades;
+
+public class Incidencia {
+
+    private int idaccidentes;
+    private  String nombre_accidente;
+    private String descripcion;
+    private String ubicacion;
+    private String foto;
+    private int idusuarios;
+
+    public int getIdaccidentes() {
+        return idaccidentes;
+    }
+
+    public void setIdaccidentes(int idaccidentes) {
+        this.idaccidentes = idaccidentes;
+    }
+
+    public String getNombre_accidente() {
+        return nombre_accidente;
+    }
+
+    public void setNombre_accidente(String nombre_accidente) {
+        this.nombre_accidente = nombre_accidente;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getUbicacion() {
+        return ubicacion;
+    }
+
+    public void setUbicacion(String ubicacion) {
+        this.ubicacion = ubicacion;
+    }
+
+    public String getFoto() {
+        return foto;
+    }
+
+    public void setFoto(String foto) {
+        this.foto = foto;
+    }
+
+    public int getIdusuarios() {
+        return idusuarios;
+    }
+
+    public void setIdusuarios(int idusuarios) {
+        this.idusuarios = idusuarios;
+    }
+
+
+
+}
---
+++ app/src/main/java/com/pucp/proyectogrupo6/ListaIncidenciasUsuarioAdapter.java
@@ -0,0 +1,70 @@
+package com.pucp.proyectogrupo6;
+
+import android.content.Context;
+import android.text.Layout;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.pucp.proyectogrupo6.Entidades.Incidencia;
+
+import org.w3c.dom.Text;
+
+public class ListaIncidenciasUsuarioAdapter extends RecyclerView.Adapter<ListaIncidenciasUsuarioAdapter.IncidenciaUsuarioViewHolder> {
+
+    Incidencia[] listaincidente;
+    Context contexto;
+
+    public ListaIncidenciasUsuarioAdapter(Incidencia[] lista, Context c) {
+
+        this.listaincidente = lista;
+        this.contexto = c;
+    }
+
+
+
+
+    public static class IncidenciaUsuarioViewHolder extends RecyclerView.ViewHolder{
+        TextView  id_incidencia;
+        Context context;
+        public IncidenciaUsuarioViewHolder(View itemView){
+            super(itemView);
+            context = itemView.getContext();
+            id_incidencia = itemView.findViewById(R.id.Id_incidencia);
+
+        }
+
+    }
+    @NonNull
+    @Override
+    public IncidenciaUsuarioViewHolder onCreateViewHolder( ViewGroup parent, int viewType) {
+        View item = LayoutInflater.from(contexto).inflate(R.layout.rv_usuario,parent,false);
+        IncidenciaUsuarioViewHolder incidenciaUsuarioViewHolder = new IncidenciaUsuarioViewHolder(item);
+        return incidenciaUsuarioViewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder( IncidenciaUsuarioViewHolder holder, int position) {
+    Incidencia incidencia = listaincidente[position];
+    String texto  = incidencia.getNombre_accidente();
+    holder.id_incidencia.setText(texto);
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return listaincidente.length;
+    }
+
+
+
+
+}
