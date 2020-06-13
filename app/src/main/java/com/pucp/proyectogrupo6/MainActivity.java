431a9fdddb7b580fa8bf04b2ad92d19f664bcad3
Josue <jlopezc@pucp.edu.pe>
2020-06-13 14:02:50 -0500

    firebase

--- app/src/main/java/com/pucp/proyectogrupo6/MainActivity.java
+++ app/src/main/java/com/pucp/proyectogrupo6/MainActivity.java
@@ -1,8 +1,15 @@
 package com.pucp.proyectogrupo6;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +18,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        DatabaseReference databaseReference= FirebaseDatabase.getInstance().getReference();
+        Cursos cursos=new Cursos();
+        cursos.setApellido("Lopezc");
+        cursos.setNombre("campos");
+
+        databaseReference.setValue(cursos).addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+                Log.d("infoApp","guardado exitosamente");
+
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.d("infoapp","error");
+            }
+        });
 
 
     }
